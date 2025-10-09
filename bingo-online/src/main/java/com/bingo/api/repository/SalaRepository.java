06970a91a0e5f6eea2194da8a8e535af0fcf6dae
L30-R1B <lr82460@gmail.com>
2025-10-09 13:46:21 -0300

    limpando projeto

--- bingo-online/src/main/java/com/bingo/api/repository/SalaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bingo.api.repository;
-
-import com.bingo.api.entity.Sala;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SalaRepository extends JpaRepository<Sala, Long> {
-}
--- commit.sh
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m"alterações feitas"
-git push origin main
